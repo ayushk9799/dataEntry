79ea07847b7868ab368b7dd33d23e4932929cd73
Ayush Kumar <ayushk2.ug19.cs@nitp.ac.in>
2024-06-16 17:40:09 +0530

    new scrapper

--- job.js
+++ job.js
@@ -29,38 +29,49 @@ export const test=async(url)=> {
       });
   const page = await browser.newPage();
   await page.setViewport({ width: 1280, height: 800 });
+  await page.setRequestInterception(true);
+  function extractUsername(url) {
+    const regex = /instagram\.com\/([^/?#]+)/;
+    const match = url.match(regex);
+    return match ? match[1] : null;
+}
+const username=extractUsername(url);
 
-  let userId;
+  page.on('request', (request) => {
+    request.continue();
+  });
+
+  let apiResponseData = {};
+
+  page.on('response', async (response) => {
+    try {
+      const requestUrl = response.url();
+      if (requestUrl.includes('https://www.instagram.com/api/graphql')) {  
+        const responseData = await response.json();
+        apiResponseData = responseData; 
+        console.log(apiResponseData)
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
   await page.goto(url, { waitUntil: "networkidle0",timeout:60000 });
 
-  const result = await page.evaluate(async() => {
-    const userIDClass = document.getElementsByClassName(
-"x1lliihq x1plvlek xryxfnj x1n2onr6 x193iq5w xeuugli x1fj9vlw x13faqbe x1vvkbs x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x1i0vuye x1ms8i2q xo1l8bm x5n08af x10wh9bi x1wdrske x8viiok x18hxmgj"    );
-    console.log(userIDClass)
-    const name = document.getElementsByClassName(
-      "x1lliihq x1plvlek xryxfnj x1n2onr6 x193iq5w xeuugli x1fj9vlw x13faqbe x1vvkbs x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x1i0vuye xvs91rp x1s688f x5n08af x10wh9bi x1wdrske x8viiok x18hxmgj"  )       
-   
-   
-      userId = userIDClass[0]?.textContent;
-    const detailsClass = document.getElementsByClassName(
-      "html-span xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x1hl2dhg x16tdsg8 x1vvkbs"
-    );
-    const imageElement = document.getElementsByClassName('xpdipgo x972fbf xcfux6l x1qhh985 xm0m39n xk390pu x5yr21d xdj266r x11i5rnm xat24cr x1mh8g0r xl1xv1r xexx8yu x4uap5 x18d9i69 xkhd6sd x11njtxf xh8yej3')[0];
-    const imageSrc = imageElement ? imageElement.src : null;
 
-    console.log(imageSrc);
-    console.log("hello")
-    return {
-        iaccountID:userId,
-        ifollowers: detailsClass[1]?.textContent,
-        name:name[0]?.textContent,
-        profilePic:imageSrc,
+await new Promise((resolve)=>setTimeout(resolve,5000));
+    const result={
+      iaccountID:apiResponseData.data.user.username,
+      ifollowers: apiResponseData.data.user.follower_count,
+      name:apiResponseData.data.user.full_name,
+              profilePic:apiResponseData.data.user.hd_profile_pic_url_info.url
     }
-
-});
-console.log(result)
+    console.log(result.profilePic)
     result.profilePic=await downloadImage(result.profilePic);
-    await browser.close();
+    console.log(result.profilePic)
+
+     await browser.close();
+    console.log(result)
    return result;
    
 }
